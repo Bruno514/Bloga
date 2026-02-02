bbde5d4ef1436c13ea856902f095277bccda12fd
Bruno <86791361+Bruno514@users.noreply.github.com>
2026-02-02 14:24:30 -0300

    Add button variants

--- site/publishers/src/components/Button/Button.jsx
+++ site/publishers/src/components/Button/Button.jsx
@@ -1,12 +1,11 @@
 import styles from "./Button.module.scss";
 
-export function Button({ children, onClick, variant, type, className = [] }) {
-  const buttonStyle = [styles.button, styles[variant]]
-    .concat(className)
-    .join(" ");
+export function Button({ children, onClick, style, kind }) {
+  const variant =
+    styles[`${kind}${style.charAt(0).toUpperCase() + style.slice(1)}`];
   return (
     <>
-      <button className={buttonStyle} onClick={onClick} type={type}>
+      <button className={variant} onClick={onClick}>
         {children}
       </button>
     </>
