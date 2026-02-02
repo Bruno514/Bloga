f00fea6800634c4fdb7ca7571854a5faee23e789
Bruno <86791361+Bruno514@users.noreply.github.com>
2026-02-02 14:43:40 -0300

    Handle ApiError

--- site/publishers/src/services/articleService.js
+++ site/publishers/src/services/articleService.js
@@ -1,32 +1,25 @@
+import ApiError from "@/errors/ApiError.js";
+
 export const postArticle = async (title, content, published = false) => {
   const url = `${import.meta.env.VITE_API_URL}/article`;
-  console.log("Title: ", title);
-  console.log("Content: ", content);
-  console.log("Published: ", published);
 
-  console.log(JSON.stringify({ title, content, published }));
-  try {
-    const token = localStorage.getItem("token");
+  const token = localStorage.getItem("token");
 
-    const response = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-      body: JSON.stringify({ title, content, published }),
-    });
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({ title, content, published }),
+  });
 
-    const result = await response.json();
+  const result = await response.json();
 
-    if (response.ok) return result;
+  if (response.ok) return result;
 
-    switch (response.status) {
-      default:
-        throw new Error(response.statusText);
-    }
-  } catch (error) {
-    console.error(error);
-    throw error;
+  switch (response.status) {
+    default:
+      throw new ApiError(result?.error || response.statusText, response.status);
   }
 };
